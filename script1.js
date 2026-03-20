4b403b529c3577849e37541bac9cf3c0d84b57c4
Shivakumar <bashamonishivakumar081@gmail.com>
2026-03-20 19:03:22 +0530

    practice

---
+++ script1.js
@@ -0,0 +1,46 @@
+let taskInput = document.getElementById("newTask");
+let button = document.getElementById("addButton");
+let taskList = document.getElementById("taskList");
+let clearButton = document.getElementById("clearButton");
+
+let todos = [
+  { id: 1, text: "hello world" },
+  { id: 2, text: "learn javascript" },
+];
+//read
+function todosList() {
+  let task = "";
+  for (i = 0; i < todos.length; i++) {
+    task +=
+      todos[i].id +
+      "." +
+      todos[i].text +
+      "<br>" +
+      "<span><button type='button' onclick='removeTodo(" +
+      todos[i].id +
+      ")'>Delete</button></span>" +
+      "<br>";
+  }
+  taskList.innerHTML = task;
+  taskInput.value = "";
+}
+
+//create
+
+button.onclick = function () {
+  task = taskInput.value;
+  if (task === "") {
+    return;
+  }
+
+  todos.push({ id: todos.length + 1, text: task });
+  taskInput.value = "";
+  todosList();
+};
+//delete
+function removeTodo(id) {
+  todos = todos.filter(function (subject) {
+    return subject.id !== id;
+  });
+  todosList();
+}
